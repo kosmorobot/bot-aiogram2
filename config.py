a21a53dcf9545d91249ce103759c4cc4feb1cd97
My Name <alex971568906558@gmail.com>
2021-08-04 14:36:57 +0300

    3

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 open_weather_token = "a20fbd1804c45c9ec3543a501b38c46e"
+tg_bot_token = "1906763910:AAFdqk2x7clfb8fwg-95P4rTmYLQKs36Ssk"
---
+++ main_weather_tg_bot.py
@@ -0,0 +1,10 @@
+import requests
+import datetime
+from config import tg_bot_token, open_weather_token
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+
+bot = Bot(token=tg_bot_token)
+
